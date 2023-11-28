e84c8e1d400789fe55717f101ac35f4c2e43bdf3
shiyunjin <admin@shiyunjin.cn>
2023-11-28 18:04:11 +0800

    fix: ucloud_us3 support sign method

--- ucloud_us3/sign.go
+++ ucloud_us3/sign.go
@@ -1,29 +1,69 @@
 package ucloud_us3
 
 import (
-	"github.com/pkg/errors"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
+	ufsdk "github.com/ufilesdk-dev/ufile-gosdk"
 )
 
 func (o *UCloudUS3) SignGetUrl(name string, expiredInSec int64) (string, error) {
-	//auth := ufsdk.NewAuth(o.config.AK, o.config.SK)
-	//
-	//auth.AuthorizationPrivateURL("POST", o.config.Bucket, name, nil)
+	req, err := ufsdk.NewFileRequest(o.clientConfig, nil)
+	if err != nil {
+		return "", err
+	}
 
-	return "", errors.New("not support")
+	signUrl := req.GetPrivateURL(name, time.Duration(expiredInSec)*time.Minute)
+
+	return signUrl, nil
 }
 
 func (o *UCloudUS3) SignPutUrl(name string, expiredInSec int64) (string, error) {
-	return "", errors.New("not support")
+	auth := ufsdk.NewAuth(o.config.AK, o.config.SK)
+
+	t := time.Now()
+	t = t.Add(time.Duration(expiredInSec) * time.Second)
+	expires := strconv.FormatInt(t.Unix(), 10)
+
+	sign, publicKey := auth.AuthorizationPrivateURL("PUT", o.config.Bucket, name, expires, nil)
+
+	req, err := ufsdk.NewFileRequest(o.clientConfig, nil)
+	if err != nil {
+		return "", err
+	}
+
+	query := url.Values{}
+	query.Add("UCloudPublicKey", publicKey)
+	query.Add("Signature", sign)
+	query.Add("Expires", expires)
+	reqURL := req.GetPublicURL(name)
+	return reqURL + "?" + query.Encode(), nil
 }
 
 func (o *UCloudUS3) SignPutUrlWithMD5(name string, expiredInSec int64, md5 string) (string, error) {
-	//auth := ufsdk.NewAuth(o.config.AK, o.config.SK)
-	//
-	//h := make(http.Header)
-	//h.Add("Content-Type", "application/octet-stream")
-	//h.Add("Content-MD5", md5)
-	//
-	//signedURL := auth.Authorization("POST", o.config.Bucket, name, h)
+	auth := ufsdk.NewAuth(o.config.AK, o.config.SK)
 
-	return "", errors.New("not support")
+	t := time.Now()
+	t = t.Add(time.Duration(expiredInSec) * time.Second)
+	expires := strconv.FormatInt(t.Unix(), 10)
+
+	h := http.Header{}
+	h.Set("Content-MD5", md5)
+
+	sign, publicKey := auth.AuthorizationPrivateURL("PUT", o.config.Bucket, name, expires, h)
+
+	req, err := ufsdk.NewFileRequest(o.clientConfig, nil)
+	if err != nil {
+		return "", err
+	}
+
+	query := url.Values{}
+	query.Add("UCloudPublicKey", publicKey)
+	query.Add("Signature", sign)
+	query.Add("Expires", expires)
+	reqURL := req.GetPublicURL(name)
+
+	return reqURL + "?" + query.Encode(), nil
 }
